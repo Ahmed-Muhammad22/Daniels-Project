c3db3683926417091e1ae2e123a8e69ba4fd5b00
Ahmed-Muhammad22 <ahmed.khater22698@gmail.com>
2025-03-19 00:33:11 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//!---------- typed js library---------!//
+var typed = new Typed("#element", {
+  strings: ["Larry Daniels",  "Developer",  "Designer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+//!---------- typed js library---------!//
